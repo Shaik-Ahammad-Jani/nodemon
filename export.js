002307b6e58aca43a7c29d8366d448a06f8d57e7
Shaik Ahammad Jani <shaikjani4403@gmail.com>
2025-03-12 15:17:15 +0530

    first commit

---
+++ export.js
@@ -0,0 +1,3 @@
+export function myFunction() {
+	console.log('this is a function ')
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import { myFunction } from './export.js'
+console.log('hooooooi')
+console.log(myFunction())
+console.log('hii')
